d1334e6968e405bdaf0f9298e9c4cd9bd5fed4ff
Sammo98 <75253485+Sammo98@users.noreply.github.com>
2021-07-31 09:47:21 +0100

    Add files via upload

--- follower_count_csv_maker.py
+++ follower_count_csv_maker.py
@@ -1,20 +1,11 @@
-import os, random, sys, time, json
+import random, time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from openpyxl import load_workbook
-import re
-import requests
 import pandas as pd
 
-
-"""Random Sleep Functions"""
-def random_long_sleep(): # Random long sleep function (10-30 seconds) to emulate human behaviour
-    time.sleep(random.randint(10,30))
-    
-def random_short_sleep(): # Random short sleep function (5-10 seconds) to emulate human behaviour
-    time.sleep(random.randint(5,8)) 
 
 def login(config_file): # Function to get username/password from config file and submit to linkedin
 
@@ -26,7 +17,7 @@ def login(config_file): # Function to get username/password from config file and
     try: # Try 
         username_box = driver.find_element_by_id("username") # Locate Username Box
         username_box.send_keys(username) # Send username to username box
-        random_short_sleep() # Short sleep
+        time.sleep(random.randint(5,8))
 
         password_box = driver.find_element_by_id("password") # Locate Password Box
         password_box.send_keys(password) # Send password to password box
@@ -39,29 +30,29 @@ def get_url_list(file_name):
         url_list = f.readlines()
     return url_list
 
-def get_follower_count(url): # Get Follower Count from A Browser Source:
-    driver.get(url)
-    random_long_sleep()
-    info_elements = driver.find_elements_by_xpath("//div[@class='org-top-card-summary-info-list__info-item']") 
-    for element in info_elements:
-        if "follower" in element.text:
-            follower_count = int(element.text.split()[0].replace(",",""))
+def get_follower_count(url): # Get Follower Count From Browser Source
+    driver.get(url) # Navigate Browser to URL
+    time.sleep(random.randint(10,30)) # Sleep
+    soup = BeautifulSoup(driver.page_source, "lxml") # Create Soup Object
+    info_elements = soup.find_all("div", class_="org-top-card-summary-info-list__info-item") # Find Info Elements
+    for element in info_elements: # Iterate Elements
+        if "follower" in element.text: # If Follower Found in the text (i.e the follower count element)
+            follower_count = int(element.text.split()[0].replace(",","")) # Munge Follower Count to int
         else:
             pass
 
-    random_short_sleep()
-
-    if follower_count != None:
+    if follower_count != None: # If Follower Count was found, pass
         pass
-    else:
+    else: # If not, set follower_count to None and print "Not Found"
         follower_count = None
+        print("Follower Count Not Found") 
     return follower_count
 
 def iterate_company_url_tocsv(company_url_list, filename): # Iterate Company url list and save as a csv fileyeh ex
     follower_count_list = []
 
     for url in company_url_list:
-        print(f"Follower Count Currently being scraped for {url}:")
+        print(f"Follower Count Currently being scraped for {url}")
         follower_count = get_follower_count(url)
         print(f"The Follower Count is: {follower_count}")
         follower_count_list.append(follower_count)
@@ -69,13 +60,21 @@ def iterate_company_url_tocsv(company_url_list, filename): # Iterate Company url
     df = pd.DataFrame({"Follower Count":follower_count_list})
     df.to_csv(filename, index = False)
 
+def main():
 
-driver = webdriver.Chrome("/Users/sam/Documents/Scripts/LinkedIn/driver/chromedriver") #Initiate Driver
-driver.get("https://www.linkedin.com/uas/login") # Get Driver to open LinkedIn Login Page
-login("config.txt") # Login to linkedin
+    global driver
 
-bankrupt_url_list = get_url_list("bankrupt file")
-live_url_list = get_url_list("live file")
+    driver = webdriver.Chrome("/Users/sam/Documents/Scripts/LinkedIn/driver/chromedriver") #Initiate Driver
+    driver.get("https://www.linkedin.com/uas/login") # Get Driver to open LinkedIn Login Page
+    login("config.txt") # Login to linkedin
 
-iterate_company_url_tocsv(bankrupt_url_list, "bankrupt_follower_count.csv")
-iterate_company_url_tocsv(live_url_list, "live_follower_count.csv")
+    bankrupt_url_list = get_url_list("bankrupt_url.txt")
+    live_url_list = get_url_list("live_url.txt")
+
+    iterate_company_url_tocsv(bankrupt_url_list, "bankrupt_follower_count.csv")
+    iterate_company_url_tocsv(live_url_list, "live_follower_count.csv")
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
--- linkedin_company_url_maker.py
+++ linkedin_company_url_maker.py
@@ -1,18 +1,10 @@
-import os, random, sys, time, json
+import random, time, re
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
 from openpyxl import load_workbook
-import re
-import requests
 import pandas as pd
-
-def random_long_sleep(): # Random long sleep function (10-30 seconds) to emulate human behaviour
-    time.sleep(random.randint(10,30))
-    
-def random_short_sleep(): # Random short sleep function (5-10 seconds) to emulate human behaviour
-    time.sleep(random.randint(5,8)) 
 
 def login(config_file): # Function to get username/password from config file and submit to linkedin
 
@@ -24,7 +16,7 @@ def login(config_file): # Function to get username/password from config file and
     try: # Try 
         username_box = driver.find_element_by_id("username") # Locate Username Box
         username_box.send_keys(username) # Send username to username box
-        random_short_sleep() # Short sleep
+        time.sleep(random.randint(5,8))
 
         password_box = driver.find_element_by_id("password") # Locate Password Box
         password_box.send_keys(password) # Send password to password box
@@ -32,7 +24,7 @@ def login(config_file): # Function to get username/password from config file and
     except: # except for auto login
         pass
 
-def get_excel_column (file):  #Function to Get t"""Function to Get the Company Name Column of Pitchbook Exported Data"""whe Company Name Column of Pitchbook Exported Data
+def get_excel_column (file):  #Function to Get the Company Name Column of Pitchbook Exported Data"""whe Company Name Column of Pitchbook Exported Data
     wb = load_workbook(file) # Open Live companies workbook
     live_companies = wb["Data"] # Navigate to correct sheet 
     column = live_companies['B'] # Column B
@@ -42,19 +34,18 @@ def get_company_url(search_query): # This will search a company and then retriev
 
     linkedin_search_box = driver.find_element_by_xpath("//input[@aria-label='Search']") # Locate Search Box
     linkedin_search_box.send_keys(search_query) # Enter search query into search box
-    random_short_sleep() # Sleep
+    time.sleep(random.randint(5,8)) # Sleep
     linkedin_search_box.send_keys(Keys.ENTER) # "Press" enter key
-    random_short_sleep() # Sleep
-    linkedin_search_box.clear()
+    time.sleep(random.randint(5,8)) # Sleep
+    linkedin_search_box.clear() # Clear Search Box
     try:
         companies_button = driver.find_element_by_xpath('//button[@aria-label="Companies"]') # Locate Companies Button on search page
         companies_button.click() # Click Button
     except:
         pass
-    random_short_sleep() # Sleep
+    time.sleep(random.randint(5,8)) # Sleep
     
-    source = driver.page_source # Retrieve source code from search page
-    soup = BeautifulSoup(source, "lxml") # Make soup object
+    soup = BeautifulSoup(driver.page_source, "lxml") # Make soup object
     results = soup.find("div", {"class":'ph0 pv2 artdeco-card mb2'}) # Locate results list
     
     if results != None:
@@ -66,21 +57,25 @@ def get_company_url(search_query): # This will search a company and then retriev
             top_result_url = driver.current_url
     else: 
         top_result_url = None
-        
+
+    time.sleep(random.randint(12,18))    
     return top_result_url # Return URL
 
-def create_url_file(company_name_list, file_name):
-    url_list = []
-    for company in company_name_list:
+def create_url_file(company_name_list, file_name): # Function to create a .txt file of company URLs and save with a given filename
+    url_list = [] # Init Empty List
+    for company in company_name_list: # Iterate Company Names
         print(f"Getting URL for {company}")
-        url = get_company_url(company)
+        url = get_company_url(company) # Get url for that company
+
         if url == None: # If get_company_url returns none try running function again
             print("No URL was found for this company, reattempting to find again")
-            random_long_sleep()       
+            time.sleep(random.randint(10,30))     
             url = get_company_url(company)
+
         print(f"The URL for this company is {url}") # Print url
         url_list.append(url) # Append to list
-        random_short_sleep()
+        time.sleep(random.randint(5,8))
+
         if len(url_list) % 7 == 0: # Add random long break every 7 companies scraped in order to avoid linkedin maximum search frequency
             time.sleep(random.randint(60,120))
 
@@ -88,20 +83,26 @@ def create_url_file(company_name_list, file_name):
         for item in url_list:
             f.write(f"{item}\n")
 
-live_list = get_excel_column("live_companies.xlsx") # Create Live Company List
-bankrupt_list = get_excel_column("bankrupt_companies.xlsx") # Create Bankrupt Company list
-live_company_list = [re.sub("\(.*\)", "", live_list[i].value) for i in range(9,len(live_list)) if live_list[i].value != None] # Munge
-bankrupt_company_list = [re.sub("\(.*\)", "", bankrupt_list[i].value) for i in range(9,len(bankrupt_list)) if bankrupt_list[i].value != None] #Munge
+def main(): # Main Function
 
-driver = webdriver.Chrome("/Users/sam/Documents/Scripts/LinkedIn/driver/chromedriver") #Initiate Driver
-driver.get("https://www.linkedin.com/uas/login") # Get Driver to open LinkedIn Login Page
-login("config.txt") # Login to linkedin
+    global driver # Create Global Driver
 
-create_url_file(bankrupt_company_list, "bankrupt_url.txt")
-create_url_file(live_company_list, "live_url.txt")
+    live_list = get_excel_column("live_companies.xlsx") # Create Live Company List
+    bankrupt_list = get_excel_column("bankrupt_companies.xlsx") # Create Bankrupt Company list
+    live_company_list = [re.sub("\(.*\)", "", live_list[i].value) for i in range(9,len(live_list)) if live_list[i].value != None] # Munge Company List
+    bankrupt_company_list = [re.sub("\(.*\)", "", bankrupt_list[i].value) for i in range(9,len(bankrupt_list)) if bankrupt_list[i].value != None] #Munge Company List
 
-driver.quit()
+    driver = webdriver.Chrome("/Users/sam/Documents/Scripts/LinkedIn/driver/chromedriver") #Initiate Driver
+    driver.get("https://www.linkedin.com/uas/login") # Get Driver to open LinkedIn Login Page
+    login("config.txt") # Login to linkedin
 
+    create_url_file(bankrupt_company_list, "bankrupt_url.txt") # Create URL txt file for bankrupt companies (calls get_company_url within)
+    create_url_file(live_company_list, "live_url.txt" ) # Create URL txt file for live companies (calls get_company_url within)
+
+    driver.quit() # Quite Driver
+
+if __name__ == "__main__": # Executable Script as Main Program
+    main()
 
 
 
